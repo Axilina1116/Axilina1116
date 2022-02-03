90bd4f277b4ac1bb14ac8a7c2e178bfa8889e68a
Axilina1116 <98871105+Axilina1116@users.noreply.github.com>
2022-02-03 19:25:30 +0530

    Add files via upload

---
+++ program5.java
@@ -0,0 +1,11 @@
+
+public class Byte {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+short shortNumber = 150;
+byte byteNumber = (byte) shortNumber;
+System.out.println("byteNumber: " +byteNumber);
+	}
+
+}
